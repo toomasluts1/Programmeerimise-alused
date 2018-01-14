333221ad011d643b2c700754dfce8cf1f1de9671
toomasluts1 <toomasluts1@gmail.com>
2018-01-14 12:52:08 +0200

    Game of blackjack

---
+++ BlackJackHomeWork/BlackJack/BJPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class BJPlayer
+    {
+        private int score = new int();
+        private List<string> hand = new List<string>();
+
+        public void TakeCard(string C)
+        {
+            hand.Add(C);
+        }
+
+        public string ShowCard(int i)
+        {
+            return hand[i - 1];
+        }
+
+        public void ShowHand()
+        {
+            for(int i=0; i<hand.Count(); i++)
+            {
+                if (i != 0) Console.Write(", ");
+                Console.Write(hand[i]);
+            }
+        }
+
+        public void AddScore(int n)
+        {
+            score = score + n;
+        }
+
+        public int ShowScore()
+        {
+            return score;
+        }
+    }
+}
