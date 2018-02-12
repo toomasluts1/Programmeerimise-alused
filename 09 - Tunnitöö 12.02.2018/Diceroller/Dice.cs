18d73b8e285aeca0d4960a61f64f1920ee99b565
Tehnikumi Opilane <opilane@tptlive.ee>
2018-02-12 15:58:58 +0200

    Diceroll

---
+++ 09 - Tunnitöö 12.02.2018/Diceroller/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    class Dice
+    {
+        Random rnd = new Random();
+
+        public int Roll(int tahud)
+        {
+            return rnd.Next(1, tahud + 1);
+        }
+    }
+}
---
+++ 09 - Tunnitöö 12.02.2018/Diceroller/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+            int sum = 0;
+
+            for(int i = 0; i<3; i++)
+            {
+                int num = rnd.Next(1, 7);
+                sum = sum + num;
+                Console.WriteLine($"1d6: {num}");
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                int num = rnd.Next(1, 9);
+                sum = sum + num;
+                Console.WriteLine($"1d8: {num}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Roll total: {sum}");
+            Console.WriteLine();
+            Console.WriteLine(">");
+
+            Console.ReadKey();
+        }
+    }
+}
