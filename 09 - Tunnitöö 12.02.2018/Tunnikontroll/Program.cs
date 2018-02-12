a510c5f47d023523d06f4d498fe47c9a990fdb57
Tehnikumi Opilane <opilane@tptlive.ee>
2018-02-12 11:20:08 +0200

    Lausemixer

---
+++ 09 - Tunnitöö 12.02.2018/Tunnikontroll/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnikontroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sisesta lause: ");
+            string lause = Console.ReadLine();
+            string[] sõnad = lause.Split(' ');
+            var uuedsõnad = new List<string>();
+
+            foreach(string sõna in sõnad)
+            {
+                if(sõna.Length > 3)
+                {
+                    char algustäht = sõna[0];
+                    char lõpptäht = sõna[sõna.Length - 1];
+                    string str1 = sõna.Remove(0, 1);
+                    string keskosa = str1.Remove(str1.Length - 1, 1);
+                    string mixedkeskosa = keskosa;
+
+                    while(keskosa == mixedkeskosa)     //Kui tagastatakse juhuslikult sama sõna, mixi uuesti
+                    {
+                        mixedkeskosa = Shuffle.StringMixer(keskosa);
+                    }
+
+                    string uussõna = string.Join("", algustäht, mixedkeskosa, lõpptäht);
+                    uuedsõnad.Add(uussõna);
+                }
+
+                else
+                {
+                    uuedsõnad.Add(sõna);
+                }
+            }
+
+            string uuslause = string.Join(" ", uuedsõnad);
+
+            Console.WriteLine();
+            Console.WriteLine(uuslause);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 09 - Tunnitöö 12.02.2018/Tunnikontroll/Shuffle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnikontroll
+{
+    class Shuffle  //Fisher - Yates shuffle, Võetud siit: https://stackoverflow.com/questions/4739903/shuffle-string-c-sharp
+    {
+        static System.Random rnd = new System.Random();
+
+        static void Fisher_Yates(int[] array)
+        {
+            int arraysize = array.Length;
+            int random;
+            int temp;
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                random = i + (int)(rnd.NextDouble() * (arraysize - i));
+
+                temp = array[random];
+                array[random] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        public static string StringMixer(string s)
+        {
+            string output = "";
+            int arraysize = s.Length;
+            int[] randomArray = new int[arraysize];
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                randomArray[i] = i;
+            }
+
+            Fisher_Yates(randomArray);
+
+            for (int i = 0; i < arraysize; i++)
+            {
+                output += s[randomArray[i]];
+            }
+
+            return output;
+        }
+    }
+}
